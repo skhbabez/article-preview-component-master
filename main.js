cd65b85c4a5f3fa06f12371142d9ce0bd2919918
Sascha Khbabez <sascha.khbabez@outlook.de>
2025-07-23 16:33:22 +0200

    finished mobile design

---
+++ main.js
@@ -0,0 +1,4 @@
+const shareToast = document.getElementById("share-toast");
+const shareButton = document.getElementById("share-button");
+console.log(shareToast.getAttribute("id"));
+shareButton.addEventListener("click", (event) => {});
