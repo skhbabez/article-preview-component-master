02ad6ad631ba409b49fa3a2debacc0a7cdb4a21a
Sascha Khbabez <sascha.khbabez@outlook.de>
2025-07-23 19:23:53 +0200

    First finished version

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 const shareToast = document.getElementById("share-toast");
 const shareButton = document.getElementById("share-button");
 console.log(shareToast.getAttribute("id"));
-shareButton.addEventListener("click", (event) => {});
+shareButton.addEventListener("click", () => {
+  if (shareToast.classList.contains("hidden")) {
+    shareToast.classList.remove("hidden");
+    if (!shareButton.classList.contains("share-btn-dark")) {
+      shareButton.classList.add("share-btn-dark");
+    }
+  } else {
+    shareToast.classList.add("hidden");
+    shareButton.classList.remove("share-btn-dark");
+  }
+});
